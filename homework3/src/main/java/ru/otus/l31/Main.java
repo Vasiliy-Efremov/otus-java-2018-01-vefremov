bc493e11f12ca8a4945fcdcc836d03eb0ad83211
Vasiliy-Efremov <36116694+Vasiliy-Efremov@users.noreply.github.com>
2018-02-21 17:58:56 +0300

    Update Main.java

--- homework3/src/main/java/ru/otus/l31/Main.java
+++ homework3/src/main/java/ru/otus/l31/Main.java
@@ -3,16 +3,17 @@ package ru.otus.l31;
 import static java.util.Collections.*;
 
 public class Main {
-        MyArrayList<String> myArrayList = new MyArrayList();
-        addAll(myArrayList, "Apple", "Cherry", "Orange", "Strawberry");
-        System.out.println(Arrays.toString(myArrayList.toArray()));
+        public static void main(String[] args) {
+                MyArrayList<String> myArrayList = new MyArrayList();
+                addAll(myArrayList, "Apple", "Cherry", "Orange", "Strawberry");
+                System.out.println(Arrays.toString(myArrayList.toArray()));
 
-        MyArrayList<String> myArrayList2 = new MyArrayList();
-        addAll(myArrayList2, "Cucumber", "Potato", "Tomato", "Onion");
-        myArrayList.addAll(2, myArrayList2);
+                MyArrayList<String> myArrayList2 = new MyArrayList();
+                addAll(myArrayList2, "Cucumber", "Potato", "Tomato", "Onion");
+                myArrayList.addAll(2, myArrayList2);
 
-        copy(myArrayList, myArrayList);
-        sort(myArrayList2, reverseOrder());
+                copy(myArrayList, myArrayList);
+                sort(myArrayList2, reverseOrder());
+                }
         }
-    }
 }
